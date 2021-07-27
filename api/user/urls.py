9582456296582e877e85ac1ded4c55668d1c078c
no-gari <sofaissofa@icloud.com>
2021-07-27 20:45:32 +0900

    urls changed

--- api/user/urls.py
+++ api/user/urls.py
@@ -7,10 +7,10 @@ from api.user.views import UserRegisterView, EmailVerifierCreateView, EmailVerif
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
-    path('social_login/', UserSocialLoginView.as_view()),
+    path('social-login/', UserSocialLoginView.as_view()),
     path('register/', UserRegisterView.as_view()),
-    path('email_verifier/', EmailVerifierCreateView.as_view()),
-    path('email_verifier/confirm/', EmailVerifierConfirmView.as_view()),
-    path('phone_verifier/', PhoneVerifierCreateView.as_view()),
-    path('phone_verifier/confirm/', PhoneVerifierConfirmView.as_view()),
+    path('email-verifier/', EmailVerifierCreateView.as_view()),
+    path('email-verifier/confirm/', EmailVerifierConfirmView.as_view()),
+    path('phone-verifier/', PhoneVerifierCreateView.as_view()),
+    path('phone-verifier/confirm/', PhoneVerifierConfirmView.as_view()),
 ]
