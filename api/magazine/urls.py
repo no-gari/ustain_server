9e85ab1aeb3d856035c865be3bafbb2f6f90cd5f
no-gari <sofaissofa@icloud.com>
2021-08-09 13:18:09 +0900

    added urls for magazine

--- api/magazine/urls.py
+++ api/magazine/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from api.magazine.views import *
 
 urlpatterns = [
-    # path('login/', TokenObtainPairView.as_view()),
+    path('magazines-list/', MagazinesListView.as_view()),
+    path('magazine/<int:id>/', MagazineRetrieveView.as_view()),
+    path('magazine/<int:id>/like-users/', MagazineLikeUserListView.as_view()),
+    path('magazine/<int:id>/create-like/', MagazineLikeCreateView.as_view()),
+    path('magazine/<int:id>/delete-like/', MagazineLikeDeleteView.as_view()),
+    path('magazine/<int:id>/reviews/', MagazineReviewsListSerializer.as_view()),
+    path('magazine/<int:id>/review-create/', MagazineReviewCreateView.as_view()),
+    path('magazine/<int:id>/review-read-update-delete/', MagazineReviewRetrieveUpdateRetrieveView.as_view()),
 ]
