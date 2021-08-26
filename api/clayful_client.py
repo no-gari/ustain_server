47fef96c5616e398db5e8916022c096c9de84e4b
mye785c83 <mye785c83@gmail.com>
2021-08-26 20:36:04 +0900

    wishlist, cart 불러오기, 추가 및 삭제 기능 구현

--- api/clayful_client.py
+++ api/clayful_client.py
@@ -69,3 +69,117 @@ class ClayfulProductClient:
         except Exception as err:
             return ValidationError({'product_detail': [err.message]})
 
+
+class ClayfulWishListClient:
+    def __init__(self, auth_token):
+        self.wishlist = Clayful.WishList
+        self.options = {
+            'customer': auth_token
+        }
+        self.wishlist_id = self.get_wishlist_id()
+
+    def create_wishlist(self, **kwargs):
+        try:
+            payload = {
+                'name': kwargs['name']
+            }
+            # options = {
+            #     'customer': kwargs['customer_auth_token']
+            # }
+            response = self.wishlist.create_for_me(payload, self.options)
+            return response
+        except Exception as err:
+            return ValidationError({'create_wishlist': [err.message]})
+
+    def get_wishlist_id(self):
+        try:
+            response = self.wishlist.list_for_me(self.options)
+            if len(response.data) == 0:
+                self.create_wishlist(name='wishlist')
+                return self.get_wishlist_id()
+            return response.data[0]['_id']
+        except Exception as err:
+            raise ValidationError({'get_wishlist_id': [err.message]})
+
+    def add_item(self, **kwargs):
+        try:
+            payload = {
+                'product': kwargs['product_id']
+            }
+            response = self.wishlist.add_item_for_me(self.wishlist_id, payload, self.options)
+            return response
+        except Exception as err:
+            raise ValidationError({'add_item': [err.message]})
+
+    def delete_item(self, **kwargs):
+        try:
+            response = self.wishlist.delete_item_for_me(self.wishlist_id, kwargs['product_id'], self.options)
+            return response
+        except Exception as err:
+            raise ValidationError({'delete_item': [err.message]})
+
+    def empty_wishlist(self):
+        try:
+            response = self.wishlist.empty_for_me(self.wishlist_id, self.options)
+            return response
+        except Exception as err:
+            raise ValidationError({'empty_wishlist': [err.message]})
+
+    def get_list_products(self):
+        try:
+            response = self.wishlist.list_products_for_me(self.wishlist_id, self.options)
+            return response
+        except Exception as err:
+            raise ValidationError({'get_list_products': [err.message]})
+
+
+class ClayfulCartClient:
+    def __init__(self, auth_token):
+        self.cart = Clayful.Cart
+        self.options = {
+            'customer': auth_token
+        }
+
+    def get_cart(self):
+        try:
+            response = self.cart.get_for_me({},self.options)
+            return response
+        except Exception as err:
+            raise ValidationError({'get_cart': [err.message]})
+
+    def get_selected_items(self, **kwargs):
+        try:
+            self.options.update({
+                'items': kwargs['items']
+            })
+            response = self.cart.get_for_me({},self.options)
+            return response
+        except Exception as err:
+            raise ValidationError({'get_selected_items': [err.message]})
+
+    def add_item(self, **kwargs):
+        try:
+            payload = {
+                'product': kwargs['product_id'],
+                'variant': kwargs['variant'],
+                'quantity': kwargs['quantity']
+            }
+            response = self.cart.add_item_for_me(payload, self.options)
+            print(response.data)
+            return response
+        except Exception as err:
+            raise ValidationError({'add_item': [err.message]})
+
+    def delete_item(self, **kwargs):
+        try:
+            response = self.cart.delete_item_for_me(kwargs['item_id'], self.options)
+            return response
+        except Exception as err:
+            raise ValidationError({'delete_item': [err.message]})
+
+    def empty_cart(self):
+        try:
+            response = self.cart.empty_for_me(self.options)
+            return response
+        except Exception as err:
+            return ValidationError({'empty_cart': [err.message]})
--- api/commerce/urls.py
+++ api/commerce/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('product/', include('api.commerce.product.urls')),
+    path('wishlist/', include('api.commerce.wishlist.urls')),
+    path('cart/', include('api.commerce.cart.urls')),
 ]
