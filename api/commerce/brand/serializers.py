2d32a2bf320381173eaa5b66130ed8a2b915965d
no-gari <sofaissofa@icloud.com>
2021-08-27 17:25:48 +0900

    changed serializers

--- api/commerce/brand/serializers.py
+++ api/commerce/brand/serializers.py
@@ -2,19 +2,23 @@ from rest_framework.validators import ValidationError
 from rest_framework import serializers
 
 
-class UrlSerializer(serializers.Serializer):
-    url = serializers.URLField(read_only=True)
-
-
-class DateTimeSeirializer(serializers.Serializer):
-    raw = serializers.DateTimeField(read_only=True)
-
-
 class BrandRetrieveSerializer(serializers.Serializer):
     _id = serializers.CharField(read_only=True)
     description = serializers.CharField(read_only=True)
     name = serializers.CharField(read_only=True)
-    logo = UrlSerializer(read_only=True)
-    thumbnail = UrlSerializer(read_only=True)
-    createdAt = DateTimeSeirializer(read_only=True)
-    updatedAt = DateTimeSeirializer(read_only=True)
+    logo = serializers.SerializerMethodField(read_only=True)
+    thumbnail = serializers.SerializerMethodField(read_only=True)
+    createdAt = serializers.SerializerMethodField(read_only=True)
+    updatedAt = serializers.SerializerMethodField(read_only=True)
+
+    def get_logo(self, value):
+        return value['logo']['url']
+
+    def get_thumbnail(self, value):
+        return value['thumbnail']['url']
+
+    def get_createdAt(self, value):
+        return value['createdAt']['raw']
+
+    def get_updatedAt(self, value):
+        return value['updatedAt']['raw']
