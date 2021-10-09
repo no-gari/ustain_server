3ce4a11f9d60b400c0418bc7ae6a0fea686524d5
no-gari <sofaissofa@icloud.com>
2021-10-09 18:10:20 +0900

    changed categories

--- api/commerce/collection/serializers.py
+++ api/commerce/collection/serializers.py
@@ -4,11 +4,3 @@ from rest_framework import serializers
 class CollectionRetrieveSerializers(serializers.Serializer):
     _id = serializers.CharField(read_only=True)
     name = serializers.CharField(read_only=True)
-    # thumbnail = serializers.SerializerMethodField(read_only=True)
-    #
-    # def get_thumbnail(self, value):
-    #     try:
-    #         value = value['thumbnail']['url']
-    #         return value
-    #     except:
-    #         return {}
--- api/commerce/collection/views.py
+++ api/commerce/collection/views.py
@@ -12,11 +12,16 @@ from django.conf import settings
 @permission_classes([AllowAny])
 def get_big_collections(request, *args, **kwargs):
     clayful_brand_client = ClayfulCollectionClient()
-    response = clayful_brand_client.get_collections(parent=settings.CLAYFUL_PRODUCT_ID)
-    if response.status == 200:
-        serializer = CollectionRetrieveSerializers(response.data, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
+    try:
+        response = clayful_brand_client.get_collections(parent=settings.CLAYFUL_PRODUCT_ID)
+        large_categories = CollectionRetrieveSerializers(response.data, many=True).data
+        big_categoreis = []
+        if response.status == 200:
+            for category in large_categories:
+                categories = clayful_brand_client.get_collections(parent=category['_id'])
+                big_categoreis.append({category['name']: CollectionRetrieveSerializers(categories.data, many=True).data})
+            return Response(big_categoreis, status=status.HTTP_200_OK)
+    except:
         raise ValidationError({'error_msg': '카테고리를 가져오지 못했습니다.'})
 
 
@@ -24,9 +29,10 @@ def get_big_collections(request, *args, **kwargs):
 @permission_classes([AllowAny])
 def get_small_collections(request, *args, **kwargs):
     clayful_collection_client = ClayfulCollectionClient()
-    small_collections = clayful_collection_client.get_collections(parent=kwargs['parent'])
-    if small_collections.status == 200:
-        serializer = CollectionRetrieveSerializers(small_collections.data, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
+    try:
+        small_collections = clayful_collection_client.get_collections(parent=kwargs['parent'])
+        if small_collections.status == 200:
+            serializer = CollectionRetrieveSerializers(small_collections.data, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    except:
         raise ValidationError({'error_msg': '카테고리를 가져오지 못했습니다.'})
